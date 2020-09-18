88280857fd5aa9ddd085aa1bd372f95e07dedf90
Drew Amandolia <amandolia.drew@gmail.com>
2020-09-17 22:33:53 -0400

    hello day 2.5

--- src/components/digimon.jsx
+++ src/components/digimon.jsx
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Typography, Card, withStyles, CardActionArea, CardMedia, CardContent } from '@material-ui/core';
+
+const useStyles = theme => ({
+    root: {
+      backgroundColor: 'silver',
+      height: 500,
+      width: 350,
+    },
+
+    digiPhoto: {
+        height: 350
+    }
+
+})  
+
 
 class Digimon extends Component {
     state = {
@@ -13,12 +28,26 @@ class Digimon extends Component {
     }
 
     render() { 
+        console.log(this.state.digimon)
+        const { classes } = this.props;
+
         return (
-            <p>
-                {this.state.digimon !== null && this.state.digimon[0].name}
-            </p>
+            <React.Fragment>
+                {this.state.digimon !== null && 
+                    <Card className={classes.root}>
+                        <CardActionArea>
+                            <CardMedia className={classes.digiPhoto} image={this.state.digimon[1].img}/> 
+                            <CardContent> 
+                                <Typography variant='h3'>{this.state.digimon[1].name}</Typography>
+                                <Typography>{this.state.digimon[1].level}</Typography>
+                            </CardContent>  
+                        </CardActionArea>
+
+                    </Card>
+                }
+            </React.Fragment>
             );
     }
 }
  
-export default Digimon;
+export default withStyles(useStyles)(Digimon);
